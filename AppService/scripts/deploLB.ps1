1c6959153ec859a4f36736409551e739efce7e72
Fernando Giuliano Celiberti <fgceliberti@outlook.com>
2020-07-11 17:10:27 -0300

    final details for web app

---
+++ AppService/scripts/deploLB.ps1
@@ -0,0 +1,11 @@
+﻿$projectName = "fgclb"
+$location = "centralus"
+$adminUserName = "fgceliberti"
+$adminPassword = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
+
+$resourceGroupName = "appsrv-rg"
+$templateUri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-load-balancer-standard-create/azuredeploy.json"
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -projectName $projectName -location $location -adminUsername $adminUsername -adminPassword $adminPassword
+
+Write-Host "Press [ENTER] to continue."
